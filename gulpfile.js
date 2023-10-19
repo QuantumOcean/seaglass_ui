959d894c02d67d4a14a6f6c0ce4d648dbc3b0cf0
FredSchuck <fredschuck@gmail.com>
2023-10-19 13:22:20 -0400

    Added autoprefixer, minification, and source mapping

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const { watch } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const gulpPostcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
-function build() {
-  return src('scss/seaglass.scss')
-    .pipe(sass())
-    .pipe(dest('dist/css'))
-}
+let srcPath = 'scss/seaglass.scss';
+let destPath = 'dist/css';
 
-function watchTask() {
-  watch(['scss/seaglass.scss'], build)
-}
+gulp.task('default', function (done) {
+  gulp.src(srcPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errorToConsole: true,
+      outputStyle: 'expanded'
+    })
+    .on('error', sass.logError))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(destPath))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })) 
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(destPath))
+    .on('end', done);
+});
 
-exports.default = series(build, watchTask)
+gulp.task('watch', function () {
+  watch(srcPath, gulp.series('default'));
+});
